dc5a5362a77fd8cdb3baede5024f4a0201e4d539
kubota yuno <im.kbxynx@2106gmail.com>
2021-05-14 21:30:53 +0900

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by 久保田有乃 on 2021/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
